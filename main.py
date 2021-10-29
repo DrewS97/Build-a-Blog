fe096c40377a06dae60a105c5de71d1fa98b97c3
dstoutin97 <64677344+DrewS97@users.noreply.github.com>
2021-10-29 04:25:27 +0000

    add pages for individual blog posts

--- main.py
+++ main.py
@@ -5,29 +5,36 @@ app = Flask('app')
 
 #Set up DB
 DATABASE = 'blogPosts.db'
-db = sqlite3.connect(DATABASE)
-cursor = db.cursor() # create a cursor object
-cursor.execute('CREATE TABLE IF NOT EXISTS blogPosts (\
-ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-TITLE TEXT NOT NULL, \
-BODY TEXT NOT NULL)')
-db.commit() # save changes
-db.close()
 
-#Render Home Page
-@app.route('/')
-def start_page():
+#Use database without values
+def sql(cmd):
   conn = sqlite3.connect(DATABASE)
   cur = conn.cursor()
-  query = cur.execute('SELECT * FROM blogPosts').fetchall()
-  posts = {}
-
-  for ID, TITLE, BODY in query:
-    posts[TITLE] = BODY
-
+  query = cur.execute(cmd).fetchall()
   conn.commit()
   conn.close()
-  return render_template("index.html", posts = posts)
+  return query
+
+#Use database with values
+def sqlVal(cmd, vals=None):
+  conn = sqlite3.connect(DATABASE)
+  cur = conn.cursor()
+  query = cur.execute(cmd, vals).fetchall()
+  conn.commit()
+  conn.close()
+  return query
+
+#Render Home Page With All Posts Or Indiviual When Clicked
+@app.route('/')
+def start_page():
+  query = sql('SELECT * FROM blogPosts')
+  id = request.args.get('ID')
+  if id != None:
+    #Grab values
+    title = sqlVal('SELECT TITLE FROM blogPosts WHERE ID=?', (id,))
+    body = sqlVal('SELECT BODY FROM blogPosts WHERE ID=?', (id,))
+    return render_template('individualBlog.html', TITLE = title, BODY = body)
+  return render_template("index.html", query = query)
 
 #Render Page to add Post
 @app.route('/addBlogPost')
@@ -37,22 +44,18 @@ def add_blog_post():
 #Render/Re-render post confirmation or page to add post
 @app.route('/addBlogPost', methods=["POST"])
 def create_post():
+  #Input info
   title = str(request.form.get("Post Title"))
   content = str(request.form.get("Post Content"))
-  
   titleLen = len(title)
   contentLen = len(content)
-
-  conn = sqlite3.connect(DATABASE)
-  cur = conn.cursor()
-  cur.execute('INSERT INTO blogPosts (TITLE, BODY) VALUES (?, ?)', (title, content))
-  conn.commit()
-  conn.close()
 
   error = "Please enter a post title under 300 characters and post content under 1000 characters"
 
   if title != "None" and content != "None":
     if titleLen > 0 and titleLen < 300 and contentLen > 0 and contentLen < 1000:
+      #Insert into DB
+      sqlVal('INSERT INTO blogPosts (TITLE, BODY) VALUES (?, ?)', (title, content))
       return render_template("postAdded.html", title = title, content = content)
     else:
       return render_template("addBlogPost.html", error = error)
